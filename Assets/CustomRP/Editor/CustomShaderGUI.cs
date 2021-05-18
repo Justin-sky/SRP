40d4d88d169869a4460e0ca65dc5767f387c7511
Justin-sky <284139554@qq.com>
2021-05-18 14:36:27 +0800

    init

---
+++ Assets/CustomRP/Editor/CustomShaderGUI.cs
@@ -0,0 +1,181 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomShaderGUI : ShaderGUI
+{
+    MaterialEditor editor;
+    Object[] materials;
+    MaterialProperty[] properties;
+
+    bool showPresets;
+
+    bool Clipping
+    {
+        set => SetProperty("_Clipping", "_CLIPPING", value);
+    }
+    bool PremultiplyAlpha
+    {
+        set => SetProperty("_PremutiplyAlpha", "_PREMUTIPLY_ALPHA", value);
+    }
+
+    BlendMode SrcBlend
+    {
+        set => SetProperty("_SrcBlend", (float)value);
+    }
+
+    BlendMode DstBlend
+    {
+        set => SetProperty("_DstBlend", (float)value);
+    }
+
+    bool ZWrite
+    {
+        set => SetProperty("_ZWrite", value ? 1f : 0f);
+    }
+
+    bool HasProperty(string name) => FindProperty(name, properties, false) != null;
+    bool HasPremultiplyAlpha => HasProperty("_PremutiplyAlpha");
+
+    RenderQueue RenderQueue
+    {
+        set
+        {
+            foreach(Material m in materials)
+            {
+                m.renderQueue = (int)value;
+            }
+        }
+    }
+
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        base.OnGUI(materialEditor, properties);
+        editor = materialEditor;
+        materials = materialEditor.targets;
+        this.properties = properties;
+
+        EditorGUILayout.Space();
+        showPresets = EditorGUILayout.Foldout(showPresets, "Presets", true);
+        if (showPresets)
+        {
+            OpaquePreset();
+            ClipPreset();
+            FadePreset();
+            TransparentPreset();
+        }
+
+    }
+
+    //设置材质属性
+    bool SetProperty(string name, float value)
+    {
+        MaterialProperty property = FindProperty(name, properties, false);
+        if(property != null)
+        {
+            property.floatValue = value;
+            return true;
+        }
+        return false;
+    }
+
+    //同时设置关键字和属性
+    void SetProperty(string name, string keyword, bool value)
+    {
+        if(SetProperty(name, value ? 1f : 0f))
+        {
+            SetKeyword(keyword, value);
+        }
+        
+    }
+
+    //设置关键字状态
+    void SetKeyword(string keyword, bool enabled)
+    {
+        if (enabled)
+        {
+            foreach(Material m in materials)
+            {
+                m.EnableKeyword(keyword);
+            }
+        }
+        else
+        {
+            foreach(Material m in materials)
+            {
+                m.DisableKeyword(keyword);
+            }
+        }
+    }
+
+    //1,创建PresetButton方法，给每种渲染模式创建一个按钮，点击它之后可以一键配置所有相关需要调节的属性
+    bool PresetButton(string name)
+    {
+        if (GUILayout.Button(name))
+        {
+            //属性重置
+            editor.RegisterPropertyChangeUndo(name);
+            return true;
+        }
+        return false;
+    }
+
+    //2 创建OpaquePreset方法进行不透明渲染模式的材质属性一系列设置
+    void OpaquePreset()
+    {
+        if (PresetButton("Opaque"))
+        {
+            Clipping = false;
+            PremultiplyAlpha = false;
+            SrcBlend = BlendMode.One;
+            DstBlend = BlendMode.Zero;
+            ZWrite = true;
+            RenderQueue = RenderQueue.Geometry;
+        }
+    }
+
+    //3 裁剪模式，只要打开Clipping和设置渲染队列 为AlphaTest
+    void ClipPreset()
+    {
+        if (PresetButton("Clip"))
+        {
+            Clipping = true;
+            PremultiplyAlpha = false;
+            SrcBlend = BlendMode.One;
+            DstBlend = BlendMode.Zero;
+            ZWrite = true;
+            RenderQueue = RenderQueue.AlphaTest;
+        }
+    }
+
+    //4.标准透明渲染模式
+    void FadePreset()
+    {
+        if (PresetButton("Fade"))
+        {
+            Clipping = false;
+            PremultiplyAlpha = false;
+            SrcBlend = BlendMode.SrcAlpha;
+            DstBlend = BlendMode.OneMinusSrcAlpha;
+            ZWrite = false;
+            RenderQueue = RenderQueue.Transparent;
+        }
+    }
+
+    // 5. 预乘
+    void TransparentPreset()
+    {
+        if (HasPremultiplyAlpha &&  PresetButton("Transparent"))
+        {
+            Clipping = false;
+            PremultiplyAlpha = true;
+            SrcBlend = BlendMode.One;
+            DstBlend = BlendMode.OneMinusSrcAlpha;
+            ZWrite = false;
+            RenderQueue = RenderQueue.Transparent;
+        }
+    }
+
+
+
+}
